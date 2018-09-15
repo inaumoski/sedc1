c05c671831691e75833916c206f4f4faf9eed453
inaumoski <igornaumoski@gmail.com>
2018-09-15 10:28:02 +0200

    Documentation added

---
+++ _Courses/Den1/Exercise/SEDC.Agency.MultiPlatform/SEDC.Agency.MultiPlatform/Bindings.cs
@@ -0,0 +1,28 @@
+﻿using Ninject.Modules;
+using SEDC.Agency.BL;
+using SEDC.Agency.BL.Interfaces;
+using SEDC.Agency.BL.Interfaces.DataAccess;
+using SEDC.Agency.DL.CsvDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Agency.WinForms
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            //Bind<IMailSender>().To<MockMailSender>();
+            //
+            Bind<IDataRepository>().To<CsvDataRepository>();
+            Bind<ILoggingRepository>().To<Log2>();
+
+
+            //BL dependency
+            Bind<IAgencyService>().To<AgencyBLService>();
+        }
+    }
+}
